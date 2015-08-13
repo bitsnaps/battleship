39a441158ce3dfa2495e00a2bb57bc66b184c8f8
Fabian Förster <githubprojects@ffoerster.com>
2015-08-13 16:39:20 +0200

    ConnectSpec vol. 1 complete

--- src/test/groovy/component/ConnectSpec.groovy
+++ src/test/groovy/component/ConnectSpec.groovy
@@ -5,7 +5,9 @@ import ratpack.groovy.test.GroovyRatpackMainApplicationUnderTest
 import ratpack.test.ServerBackedApplicationUnderTest
 import ratpack.test.http.TestHttpClient
 import spock.lang.Specification
+import spock.lang.Stepwise
 
+@Stepwise
 class ConnectSpec extends Specification {
     ServerBackedApplicationUnderTest aut = new GroovyRatpackMainApplicationUnderTest()
     @Delegate
@@ -22,5 +24,34 @@ class ConnectSpec extends Specification {
 
         and:
         jsonSlurper.parse(response.body.inputStream)['playerId'] != null
+    }
+
+    def 'a second player can connect and gets different ID'() {
+        given:
+        post('connect')
+        String player1Id = jsonSlurper.parse(response.body.inputStream)['playerId']
+
+        when:
+        post('connect')
+        String player2Id = jsonSlurper.parse(response.body.inputStream)['playerId']
+
+        then:
+        response.statusCode == 200
+
+        and:
+        (player2Id != null) && (player1Id != player2Id)
+    }
+
+    def 'the third connection attempt is rejected'() {
+        given:
+        post('connect')
+        post('connect')
+
+        when:
+        post('connect')
+
+        then:
+        response.statusCode == 409
+
     }
 }
