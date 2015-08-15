b985a72f26b6c79c127fef95415542b57fb20710
Fabian Förster <githubprojects@ffoerster.com>
2015-08-15 22:28:52 +0200

    Add FullGameSpec with test for "connect" phase of game

---
+++ src/test/groovy/component/FullGameSpec.groovy
@@ -0,0 +1,32 @@
+package component
+
+import ratpack.groovy.test.GroovyRatpackMainApplicationUnderTest
+import ratpack.test.ServerBackedApplicationUnderTest
+import ratpack.test.http.TestHttpClient
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+/**
+ * This Spec describes a complete game
+ */
+@Stepwise
+class FullGameSpec extends Specification {
+    @Shared
+    ServerBackedApplicationUnderTest aut = new GroovyRatpackMainApplicationUnderTest()
+
+    @Shared
+    Player player1 = new Player(client: TestHttpClient.testHttpClient(aut))
+
+    @Shared
+    Player player2 = new Player(client: TestHttpClient.testHttpClient(aut))
+
+    def 'connect phase'() {
+        when: 'two players connect to the game'
+        player1.connect()
+        player2.connect()
+
+        then: 'each player will have a unique playerId'
+        player1.playerId && player2.playerId && (player2.playerId != player1.playerId)
+    }
+}
