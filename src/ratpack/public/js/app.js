9b9e5cca5ac6166b48800a3614332d89fbb11194
sehmann <sven-olaf.ehmann@epost-dev.de>
2015-08-20 14:58:10 +0200

    Fix design and get access to service members and functions.

--- src/ratpack/public/js/app.js
+++ src/ratpack/public/js/app.js
@@ -2,49 +2,57 @@
 
 angular.module('battleshipApp', [])
     .factory('Player', function($http) {
-        var playerId = '',
-            myTurn = false,
-            gamePhase = '',
-            availableShips = [],
-            field = [],
-            oppositeField = [],
-            rows = [0,1,2,3,4,5,6,7,8,9],
-            isVictory = false,
-            undamagedShips = 30;
+        var _playerId = '',
+            _myTurn = false,
+            _gamePhase = '',
+            _availableShips = [],
+            _field = [],
+            _oppositeField = [],
+            _isVictory = false,
+            _undamagedShips = 30,
+            _fire,
+            _isOpenToFire,
+            _isInConnection,
+            _connect,
+            _updateField,
+            _isWater,
+            _isShip,
+            _isHit;
 
-        return {
-            fire: function(coordinate) {
-                console.log('Try to shoot on a ship');
-                if(playerId != '' && coordinate != '') {
-                    var req = {
-                        method: 'PUT',
-                        url: 'http://localhost:5050/shoot',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'playerId' : playerId
-                        },
-                        data: { 'x': 'A', 'y' : '1' }
-                    };
-                    $http.post(req).then(function(result){
+        _fire = function(coordinate) {
+            console.log('Try to shoot on a ship');
+            if(_playerId != '' && coordinate != '') {
+                var req = {
+                    method: 'PUT',
+                    url: 'http://localhost:5050/shoot',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'playerId' : _playerId
+                    },
+                    data: { 'x': 'A', 'y' : '1' }
+                };
+                $http.post(req).then(function(result){
 
-                    },function(error){
+                },function(error){
 
-                    })
-                }
-                console.log("Coordinate: ",coordinate);
-            },
-            isOpenToFire: function() {
-                return gamePhase == 'SHOOTOUT';
-            },
-            isInConnection: function() {
-                console.log("PlayerId: [" + playerId + "], gamePhase: [" + gamePhase + "]");
-                return (playerId == '' && gamePhase == '');
-            },
-            connect: function() {
-                if (this.isInConnection()) {
-                    console.log('Try to connect');
-                    var that = this,
-                        req = {
+                })
+            }
+            console.log("Coordinate: ",coordinate);
+        };
+
+        _isOpenToFire = function() {
+            return _gamePhase == 'SHOOTOUT';
+        };
+
+        _isInConnection = function() {
+            console.log("PlayerId: [" + _playerId + "], gamePhase: [" + _gamePhase + "]");
+            return (_playerId == '' && _gamePhase == '');
+        };
+
+        _connect = function() {
+            if (_isInConnection()) {
+                console.log('Try to connect');
+                var req = {
                         method: 'POST',
                         url: 'http://localhost:5050/connect',
                         headers: {
@@ -52,62 +60,79 @@ angular.module('battleshipApp', [])
                         },
                         data: {test: 'test'}
                     };
-                    console.log("Req-Config: ", req);
+                console.log("Req-Config: ", req);
 
-                    $http.post(req.url, req.data, req).then(
-                        function (result) {
-                            console.log('Start to set data: ', result.data);
-                            that.playerId = result.data.playerId;
-                            that.gamePhase = result.data.gamePhase;
-                            that.myTurn = result.data.myTurn;
-                            that.availableShips = result.data.availableShips;
-                            that.field = that.updateField(result.data.field);
-                            that.isVictory = result.data.isVictory;
-                            that.undamagedShips = result.data.undamagedShips;
-                            console.log('Data already set: ', that.field);
-                        }).catch(function (error) {
-                            console.log(error);
-                        });
-                } else {
-                    console.log("You are already connected!")
+                $http.post(req.url, req.data, req).then(
+                    function (result) {
+                        console.log('Start to set data: ', result.data);
+                        _playerId = result.data.playerId;
+                        _gamePhase = result.data.gamePhase;
+                        _myTurn = result.data.myTurn;
+                        _availableShips = result.data.availableShips;
+                        _field = _updateField(result.data.field);
+                        _isVictory = result.data.isVictory;
+                        _undamagedShips = result.data.undamagedShips;
+                        console.log('Data already set: ', _field);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+            } else {
+                console.log("You are already connected!")
+            }
+        };
+
+        _updateField = function(positions) {
+            var rows = [];
+            for (var x = 0; x < 10; x++) {
+
+                var start, col;
+                start = x * 10;
+                col = [];
+
+                for (var i = 0; i < 10; i++) {
+                    //var found, pos = start + i;
+                    //found = field.forEach(function (currentValue) {
+                    //    if (currentValue.pos === pos) {
+                    //        return currentValue;
+                    //    }
+                    //});
+                    //if (found) {
+                    //    row[i] = found
+                    //} else {
+                    col[i] = {"pos": start + i + 1, "state": "W"}
+                    //}
                 }
-            },
-            updateField: function(positions) {
-                var rows = [];
-                for (var x = 0; x < 10; x++) {
+                rows[x] = col;
+            }
+            console.log('Finish update field.');
+            return rows;
+        };
 
-                    var start, col;
-                    start = x * 10;
-                    col = [];
+        _isWater = function(cell) {
+            return cell.state == 'W';
+        };
 
-                    for (var i = 0; i < 10; i++) {
-                        //var found, pos = start + i;
-                        //found = field.forEach(function (currentValue) {
-                        //    if (currentValue.pos === pos) {
-                        //        return currentValue;
-                        //    }
-                        //});
-                        //if (found) {
-                        //    row[i] = found
-                        //} else {
-                            col[i] = {"pos": start + i + 1, "state": "W"}
-                        //}
-                    }
-                    rows[x] = col;
-                }
-                console.log('Finish update field.');
-                return rows;
-            },
-            isWater: function(cell) {
-                return cell.state == 'W';
-            },
-            isShip: function(cell) {
-                return cell.state == 'S';
-            },
-            isHit: function(cell) {
-                return cell.state == 'X';
-            },
+        _isShip = function(cell) {
+            return cell.state == 'S';
+        };
 
+        _isHit = function(cell) {
+            return cell.state == 'X';
+        };
+
+        return {
+            fire: _fire,
+            isOpenToFire: _isOpenToFire,
+            isInConnection: _isInConnection,
+            connect: _connect,
+            updateField: _updateField,
+            isWater: _isWater,
+            isShip: _isShip,
+            isHit: _isHit,
+            playerId: function() { return _playerId; },
+            gamePhase: function() { return _gamePhase; },
+            field: function() { return _field; },
+            ships: function() { return _availableShips; }
         }
 
     })
